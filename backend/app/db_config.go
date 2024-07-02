5b125387ea1b04ebfb4a22e1a113056f3567d899
Matías Lessio <80827718+MatiasLessio@users.noreply.github.com>
2024-07-02 20:28:03 -0300

    fix db connections

--- backend/app/db_config.go
+++ backend/app/db_config.go
@@ -17,10 +17,12 @@ var (
 
 func init() {
 	// DB Connections Paramters
-	DBName := "ingsw3"    //Nombre de la base de datos local de ustedes
-	DBUser := "root"      //usuario de la base de datos, habitualmente root
-	DBPass := "root"      //password del root en la instalacion
-	DBHost := "127.0.0.1" //host de la base de datos. hbitualmente 127.0.0.1
+	DBName := "ingsw3" //Nombre de la base de datos local de ustedes
+	DBUser := "root"   //usuario de la base de datos, habitualmente root
+	DBPass := "root"   //password del root en la instalacion
+	//34.31.144.174
+
+	DBHost := "34.31.144.174" //host de la base de datos. hbitualmente 127.0.0.1
 	// ------------------------
 
 	db, err = gorm.Open("mysql", DBUser+":"+DBPass+"@tcp("+DBHost+":3306)/"+DBName+"?charset=utf8&parseTime=True")
