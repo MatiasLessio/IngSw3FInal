ccac5824be05993c85c2cb0e7de9aadbcaa4f62a
Matías Lessio <80827718+MatiasLessio@users.noreply.github.com>
2024-07-03 16:48:09 -0300

    Update db_config.go

--- backend/app/db_config.go
+++ backend/app/db_config.go
@@ -4,7 +4,6 @@ import (
 	"backend/clients/reminders"
 	"backend/clients/users"
 	"backend/models"
-	"os"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -18,18 +17,16 @@ var (
 
 func init() {
 	// DB Connections Paramters
-	DBName := os.Getenv("DB_NAME") //Nombre de la base de datos local de ustedes
-	DBUser := os.Getenv("DB_USER") //usuario de la base de datos, habitualmente root
-	DBPass := os.Getenv("DB_PASS") //password del root en la instalacion
-	DBHost := os.Getenv("DB_HOST") //host de la base de datos. hbitualmente 127.0.0.1
-	// ------------------------
-
-	// Verify environment variables
-	log.Infof("Envs: DBName: %s, DBUser: %s, DBHost: %s", DBName, DBUser, DBHost, DBPass)
-
+	/*
+		DBName := os.Getenv("DB_NAME") //Nombre de la base de datos local de ustedes
+		DBUser := os.Getenv("DB_USER") //usuario de la base de datos, habitualmente root
+		DBPass := os.Getenv("DB_PASS") //password del root en la instalacion
+		DBHost := os.Getenv("DB_HOST") //host de la base de datos. hbitualmente 127.0.0.1
+		// ------------------------
+	*/
 	log.Info("Started connecting database...")
 	//db, err = gorm.Open("mysql", DBUser+":"+DBPass+"@tcp("+DBHost+":3306)/"+DBName+"?charset=utf8&parseTime=True")
-	db, err = gorm.Open("mysql", "root:root@tcp(ingsw3-428221:us-central1:ingsw3:3306)/ingsw3?charset=utf8&parseTime=True")
+	db, err = gorm.Open("mysql", "root:root@tcp(34.31.144.174:3306)/ingsw3?charset=utf8&parseTime=True")
 	if err != nil {
 		log.Info("Connection Failed to Open")
 		log.Fatal(err)
